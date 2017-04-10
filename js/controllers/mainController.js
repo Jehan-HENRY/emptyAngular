f52488c9795031e9cf2ea419b876a14383a80f10
Jehan Henry <jehan.henry@gmail.com>
2017-04-10 16:50:49 +0200

    Quest Clear

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,4 +1,13 @@
 // MAIN CONTROLLER
 function mainController($scope) {
-    
+    $scope.newPerson = {
+        name: "",
+        lastname: "",
+        email: ""
+    };
+    $scope.persons = [];
+    $scope.addPerson = function() {
+        $scope.persons.push($scope.newPerson);
+        $scope.newPerson = {};
+    };
 }
